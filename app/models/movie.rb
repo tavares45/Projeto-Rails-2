c6414febe98a8926f6ad79614f96259d867b0557
tavares45 <161387548+tavares45@users.noreply.github.com>
2024-04-08 02:14:56 -0300

    Update movie.rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,11 @@
 class Movie < ApplicationRecord
+  before_create :generate_uuid
+
+  validates :title, uniqueness: { case_sensitive: false }
+
+  private
+
+  def generate_uuid
+    self.uuid ||= UUID.new.generate
+  end
 end
